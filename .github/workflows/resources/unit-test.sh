78831e5923644f0d58e4220ae1d5552563583327
Parameswaran Sajeenthiran <psajeendran@gmail.com>
2025-11-13 16:26:58 +0530

    test 2

--- .github/workflows/resources/unit-test.sh
+++ .github/workflows/resources/unit-test.sh
@@ -3,7 +3,7 @@
 # Function to check if pod is in Pending/Running state
 pod_status=""
 check_pod_status() {
-    pod_status=$(minikube kubectl -- get pod jasminegraph-unit-test-pod --no-headers -o custom-columns=":status.phase")
+    pod_status=$(kubectl get pod jasminegraph-unit-test-pod --no-headers -o custom-columns=":status.phase")
 
     if [[ $pod_status == "Pending" || $pod_status == "ContainerCreating" ]]; then
         echo 0
@@ -13,8 +13,8 @@ check_pod_status() {
 }
 export OMP_NUM_THREADS=1
 # mkdir coverage
-minikube kubectl --  apply -f ./k8s/configs.yaml
-minikube kubectl -- apply -f ./.github/workflows/resources/unit-test-conf.yaml
+kubectl  apply -f ./k8s/configs.yaml
+kubectl apply -f ./.github/workflows/resources/unit-test-conf.yaml
 
 timeout=60 # Set the timeout in seconds (adjust as needed)
 start_time=$(date +%s)
@@ -32,13 +32,13 @@ while [[ $(check_pod_status) == "0" ]]; do
 done
 
 echo "----------------------------- logs -----------------------------"
-minikube kubectl -- logs -f jasminegraph-unit-test-pod
+kubectl logs -f jasminegraph-unit-test-pod
 
 check_pod_status
 if [[ $pod_status != "Running" && $pod_status != "Completed" ]]; then
     echo "Pod jasminegraph-unit-test-pod is in $pod_status state"
     echo "Unit tests failed"
     echo "----------------------------- details --------------------------"
-    minikube kubectl -- describe pod jasminegraph-unit-test-pod
+    kubectl describe pod jasminegraph-unit-test-pod
     exit 1
 fi
